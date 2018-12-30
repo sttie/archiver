008e7c0056720c834def3db1fe1d724b532cb652
sttie <41449047+sttie@users.noreply.github.com>
2018-12-30 23:37:18 +0300

    Update archive.py

--- archive.py
+++ archive.py
@@ -1,93 +1,95 @@
 from binary_tree import TreeNode
 
+
+# Finding two minimal numbers from an array
 def two_minimal_numbers_array(array):
     first_min = second_min = None
     maximum = max(array)
 
-    # Первое минимальное значение
+    # First minimal value
     for index, number in enumerate(array):
         if number <= maximum:
             maximum = number
             first_min = (number, index)
     
     maximum = max(array)
-    # Второе минимальное значение, пропуская первое найденное
+    # Second minimal value
     for index, number in enumerate(array):
         if index != first_min[1]:
             if number <= maximum:
                 maximum = number
                 second_min = (number, index)
 
-    # Возвращаем кортеж вида ((m1, i1), (m2, i2)), где mn - n-ое минимальное значение, in - его индекс
     return first_min, second_min
 
+
+# Creating binary code for every symbol 
 def create_bin_codes(main_node, bin_codes, code):
     if main_node.left_child is not None:
-        # Идем влево - к символу добавляется 0
+        # If we turn left, "0" is added to current bin code
         code.append("0")
-        # Если левый ребенок - символ, то добавляем ее и ее код в bin_codes
+        # If the left child is a symbol, we add it and its code into bin_codes
         if isinstance(main_node.left_child, str):
             bin_codes[main_node.left_child] = "".join(code)
-            # Идем обратно - убираем последнюю цифру code
+            # If we go back, we delete the last code's number
             code.pop()
-        # Если нет - рекурсивно вызываем функцию с новым main_node
+        # If not - we recursively call the function with new main_node
         else:
             create_bin_codes(main_node.left_child, bin_codes, code)
-            # Идем обратно - убираем последнюю цифру code
+            # If we go back, we delete the last code's number
             code.pop()
     if main_node.right_child is not None:
-        # Идем вправо - к символу добавляется 1
+        # If we turn right, "0" is added to current bin code
         code.append("1")
-        # Если правый ребенок - символ, то добавляем ее и ее код в bin_codes
+        # If the left child is a symbol, we add it and its code into bin_codes
         if isinstance(main_node.right_child, str):
             bin_codes[main_node.right_child] = "".join(code)
-            # Идем обратно - убираем последнюю цифру code
+            # If we go back, we delete the last code's number
             code.pop()
-        # Если нет - рекурсивно вызываем функцию с новым main_node
+        # If not - we recursively call the function with new main_node
         else:
             create_bin_codes(main_node.right_child, bin_codes, code)
-            # Идем обратно - убираем последнюю цифру code
+            # If we go back, we delete the last code's number
             code.pop()
 
-source = """This is mostly to make sure my methodology is correct, but my basic question was is it worth it to check outside of a function if I need to access the function at all. I know, I know, premature optimization, but in many cases, its the difference between putting an if statement inside the function call to determine whether I need to run the rest of the code, or putting it before the function call. In other words, it takes no effort to do it one way or the other. Right now, all the checks are mixed between both, and I'd like the get it all nice and standardized.
-            The main reason I asked is because the other answers I saw mostly referenced timeit, but that gave me negative numbers, so I switched to this:"""
-# Словарь частот
-letters = {}
-# Отдельный список с частотой каждого символа
-frequency = []
-# Он нужен, так как в дальнейшем в словаре будут появляться значения
-# класса TreeNode, которые не могут сравниваться с числами
+source = input()
 
-# Заполнение словаря частот
+# Frequency dict
+letters = {}
+# A list with every symbol's frequency 
+frequency = []
+# We need it, because later we will have letters keys of TreeNode class
+# that can't be compared with ints
+
+
+# Filling the frequency dict
 for i in source:
     if not letters.get(i):
         letters[i] = 0
     letters[i] = letters[i] + 1
 
-# Заполнение списка частот
+# Filling the frequency list
 for i in letters:
     frequency.append(letters[i])
 
 # Проходимся по списку, оставляя в итоге 1 элемент
 for i in range(len(letters)-1):
-    # Возвращает кортеж вида ((m1, i1), (m2, i2)), где mn - n-ое минимальное значение, in - его индекс
+    # Returns a ((m1, i1), (m2, i2)) tuple, where mn - minimal value, in - its index
     minimals = two_minimal_numbers_array(frequency)
 
-    # Ключ первого минимального значения
+    # First element's minimal value
     key_first = list(letters.keys())[minimals[0][1]]
-    # Ключ второго минимального значения
+    # Second element's minimal value
     key_second = list(letters.keys())[minimals[1][1]]
-    # Для того, чтобы вставлять в value каждого TreeNode
+    # For every TreeNode's value
     node_key = key_first + key_second
 
-    # Удаляем старые значения
+    # Deleting old values
     frequency.remove(minimals[0][0])
     frequency.remove(minimals[1][0])
-    # Складываем значения и вставляем их на последнее место
+    # Adding values and inserting them in the end
     frequency.append(minimals[0][0] + minimals[1][0])
 
-    # Здесь выполняется проверка, чтобы в потомках TreeNode не записывались сами строки,
-    # длина которых > 1, а узлы, у которых value = этим строкам
     if len(key_first) > 1 and len(key_second) > 1:
         letters[node_key] = TreeNode(0, node_key, letters[key_first], letters[key_second])
     elif len(key_first) > 1:
@@ -97,42 +99,28 @@ for i in range(len(letters)-1):
     else:
         letters[node_key] = TreeNode(0, node_key, key_first, key_second)
 
-    # Удаляем использованные элементы
+    # Deleting used elements
     letters.pop(key_first)
     letters.pop(key_second)
 
-# Берем значение первого (и единственного) элемента
+# First element's value
 final_tree = letters[next(iter(letters))]
 
 del frequency, letters
 
-# bin_codes - словарь, который содержит бинарный код для каждого символа
+# This dict contains bin code for every symbol
 bin_codes = {}
-# Переменная, содержащая бинарный код, в зависимости от движения по дереву
+# This variable contains bin code for current symbol in the create_bin_codes function
 code = []
 
-# Вызываем функцию обхода дерева с присвоением бинарного кода символам
+# Calling the function with tree traversal
 create_bin_codes(final_tree, bin_codes, code)
 
 del final_tree, code
 
-# Кодирование строки
+# String encoding
 final_bin_string = ""
 for letter in source:
     final_bin_string += bin_codes[letter]
     
 # print(bin_codes)
-
-
-def _to_Bytes(data):
-    b = bytearray()
-    for i in range(0, len(data), 8):
-        b.append(int(data[i:i+8], 2))
-        print(b)
-    return bytes(b)
-
-# Дальше - bin_codes в файл с помощью pickle, а final_bin_string в другой
-
-# f = open("test.bin", "wb")
-# f.write(_to_Bytes(final_bin_string))
-# f.close()
